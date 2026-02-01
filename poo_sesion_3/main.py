bc46b2b7a92351af448e6232cd611c6267ce465f
altair3542 <thiagomesa21@gmail.com>
2026-02-01 11:00:52 -0500

    Actualizar main.py para usar nombres de titulares consistentes y agregar clases de caché para optimizar el rendimiento. Implementar mejoras en la clase ConsolaBanco para una mejor interacción con el usuario y manejo de cuentas bancarias.

--- poo_sesion_3/main.py
+++ poo_sesion_3/main.py
@@ -7,8 +7,8 @@ def main() -> None:
     banco = BancoService()
 
     # Semilla didáctica
-    banco.abrir_ahorros("Ana", 100000, 0.01)                 # 1% mensual
-    banco.abrir_corriente("Carlos", 20000, 50000, 5000)      # cupo 50k, cuota 5k
+    banco.abrir_ahorros("Juliana", 100000, 0.01)                 # 1% mensual
+    banco.abrir_corriente("David", 20000, 50000, 5000)      # cupo 50k, cuota 5k
 
     app = ConsolaBanco(banco)
     app.ejecutar()
